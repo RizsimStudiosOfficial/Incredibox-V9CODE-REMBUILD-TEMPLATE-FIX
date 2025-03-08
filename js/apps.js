4034b85e21748e5d7c13edac8fce85e2ddbfba61
Rizsim Studios <61947503+RizsimStudiosOfficial@users.noreply.github.com>
2025-03-07 22:28:22 -0800

    Initial commit

---
+++ js/apps.js
@@ -0,0 +1,146 @@
+// real shit mang
+versions.v1 = {
+  name: "Template", // MIX DISPLAY NAME
+  version: "1", // MIX SERVER VERSION
+  date: "2025", // MOD VERSION RELEASE YEAR
+  folder: "asset-v1/", // VERSION FOLDER
+  looptime: 5333,   // VERSION LOOPTIME
+  bpm: 90,   // BPM (just info for your versions BPM / TEMPO)
+  totalframe: 242, // YOUR TOTALFRAME (I have no idea how to make a custom one soo dont ask me about it) - Rizsim Studios
+  nbpolo: 7,  // AMOUNT OF POLOS "1 to 100"
+  bonusloopA: true,   // THIS CAN BE SET TO TRUE OR FALSE (just mess with it I guess)
+  bonusendloopA: false, // THIS CAN BE SET TO TRUE OR FALSE (just mess with it I guess)
+  colBck: "#121212",  // COLOR OF THE BACKGROUND VERSION WHEN LOADED
+  col0: "#919191",   // COLOR OF TEXT
+  col1: "#5f5f5f",   // COLOR OF THE BUTTONS BG COLOR
+  col2: "#4b4b4b",   // COLOR OF THE UNHOVERED BUTTONS BG COLOR
+  col3: "#373737",   // COLOR OF THE LOADING BAR, UNSELECTED TAB COLOR AND THE HOVERED BUTTON COLOR TOO
+  col4: "#232323",   // COLOR OF THE SELECTED MENU BUTTONS ICON
+  animearray: [{
+    // 1
+    name: "0_placeholder", // POLO JSON NAME (file extenstion NOT INCLUDED!)
+    color: "828282",  // POLO LOADING COLOR
+    uniqsnd: true // SET TO TRUE IF YOUR POLO ONLY HAS 1 LOOP IF NOT SET IT TO FALSE
+  }, {
+    // 2
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 3
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 4
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 5
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 6
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 7
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 8
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 9
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 10
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 11
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 12
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 13
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 14
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 15
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 16
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 17
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 18
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 19
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }, {
+    // 20
+    name: "0_placeholder",
+    color: "828282",
+    uniqsnd: true
+  }],
+  bonusarray: [
+    //      { // BONUS 1 (remove the slashes for the bonus to make visible in game)
+    //        name: "Bonus 1 Name", // BONUS NAME (not displayed in-game)
+    //        src: "bonus-video", // BONUS VIDEO (file extenstion NOT REQUIRED!)
+    //        code: "1,2,3,4,5", // BONUS CODE (what polos are used?) can be up to like "5" but you can change it to the number commented on the animearray!
+    //        sound: "bonus-sound", // BONUS SOUND (file extenstion NOT REQUIRED!)
+    //        aspire: "aspire-sound", // ASPIRE  (remove this line to make it silent)
+    //        loop: 2, // BONUS LOOP (just find what number fits for it idfk)
+    //      },
+    //      { // BONUS 2 (remove the slashes for the bonus to make visible in game)
+    //        name: "Bonus 2 Name", // BONUS NAME (not displayed in-game)
+    //        src: "bonus-video", // BONUS VIDEO (file extenstion NOT REQUIRED!)
+    //        code: "1,2,3,4,5", // BONUS CODE (what polos are used?) can be up to like "5" but you can change it to the number commented on the animearray!
+    //        sound: "bonus-sound", // ASPIRE  (remove this line to make it silent)
+    //        aspire: "aspire-sound", // ASPIRE  (remove this line to make it silent)
+    //        loop: 2, // BONUS LOOP (just find what number fits for it idfk)
+    //      },
+    //      { // BONUS 3 (remove the slashes for the bonus to make visible in game)
+    //        name: "Bonus 3 Name", // BONUS NAME (not displayed in-game)
+    //        src: "bonus-video", // BONUS VIDEO (file extenstion NOT REQUIRED!)
+    //        code: "1,2,3,4,5", // BONUS CODE (what polos are used?) can be up to like "5" but you can change it to the number commented on the animearray!
+    //        sound: "bonus-sound", // BONUS SOUND (file extenstion NOT REQUIRED!)
+    //        aspire: "aspire-sound", // ASPIRE  (remove this line to make it silent)
+    //        loop: 2, // BONUS LOOP (just find what number fits for it idfk)
+    //      },
+  ]
+};
